5b709ab75486594a8ba1b698e18dddf3266866ab
Armand3125 <armandjulien05@gmail.com>
2024-11-05 17:37:05 +0100

    Update bouton_rouge.py

--- bouton_rouge.py
+++ bouton_rouge.py
@@ -1,14 +1,14 @@
 import streamlit as st
 
 # Titre de l'application
-st.title("Exemple de Bouton Rouge avec Streamlit")
+st.title("Exemple de Bouton avec Couleur RGB Personnalisée")
 
-# CSS pour personnaliser le bouton en rouge
+# CSS pour personnaliser le bouton avec une couleur RGB spécifique
 st.markdown("""
     <style>
     .stButton > button {
         color: white;
-        background-color: red;
+        background-color: rgb(200, 50, 100);
         padding: 10px 20px;
         font-size: 18px;
         border-radius: 5px;
