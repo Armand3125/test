63a9f10250e6db41d247b4b0f5835d3c410eaddd
Armand3125 <armandjulien05@gmail.com>
2024-11-05 17:34:02 +0100

    Update bouton_rouge.py

--- bouton_rouge.py
+++ bouton_rouge.py
@@ -1,9 +1,22 @@
 import streamlit as st
 
 # Titre de l'application
-st.title("Exemple de Bouton avec Streamlit")
+st.title("Exemple de Bouton Rouge avec Streamlit")
 
-# Afficher un bouton
+# CSS pour personnaliser le bouton en rouge
+st.markdown("""
+    <style>
+    .stButton > button {
+        color: white;
+        background-color: red;
+        padding: 10px 20px;
+        font-size: 18px;
+        border-radius: 5px;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
+# Afficher le bouton
 if st.button("Cliquez-moi !"):
     # Message affiché lorsque le bouton est cliqué
     st.write("Hello World!")
