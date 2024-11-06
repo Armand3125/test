a76c141b9ec241a542d010a499cfce9b0735dd8c
Armand3125 <armandjulien05@gmail.com>
2024-11-06 17:04:22 +0100

    Update bouton_rouge.py

--- bouton_rouge.py
+++ bouton_rouge.py
@@ -3,34 +3,42 @@ import streamlit as st
 # Créer 3 boutons avec des couleurs spécifiques
 st.markdown("""
     <style>
-    .stButton > button {
+    .red-button > button {
+        background-color: red;
+        color: white;
         padding: 10px 20px;
         font-size: 18px;
         border-radius: 5px;
         width: 100%;
     }
-    .button-red > button {
-        background-color: red;
-        color: white;
-    }
-    .button-yellow > button {
+    .yellow-button > button {
         background-color: yellow;
         color: black;
+        padding: 10px 20px;
+        font-size: 18px;
+        border-radius: 5px;
+        width: 100%;
     }
-    .button-blue > button {
+    .blue-button > button {
         background-color: blue;
         color: white;
+        padding: 10px 20px;
+        font-size: 18px;
+        border-radius: 5px;
+        width: 100%;
     }
     </style>
 """, unsafe_allow_html=True)
 
 # Affichage des boutons avec couleurs
-if st.button("Rouge", key="red_button"):
+if st.button("Rouge", key="red_button", help="Cliquez pour choisir la couleur rouge"):
+    st.markdown('<div class="red-button"></div>', unsafe_allow_html=True)
     st.write("Vous avez choisi la couleur Rouge.")
 
-if st.button("Jaune", key="yellow_button"):
+if st.button("Jaune", key="yellow_button", help="Cliquez pour choisir la couleur jaune"):
+    st.markdown('<div class="yellow-button"></div>', unsafe_allow_html=True)
     st.write("Vous avez choisi la couleur Jaune.")
 
-if st.button("Bleu", key="blue_button"):
+if st.button("Bleu", key="blue_button", help="Cliquez pour choisir la couleur bleue"):
+    st.markdown('<div class="blue-button"></div>', unsafe_allow_html=True)
     st.write("Vous avez choisi la couleur Bleue.")
-
