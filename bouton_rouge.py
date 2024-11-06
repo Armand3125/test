6e528a80ee3c84900780fb494bfe90455b3a4b86
Armand3125 <armandjulien05@gmail.com>
2024-11-06 16:03:08 +0100

    Update bouton_rouge.py

--- bouton_rouge.py
+++ bouton_rouge.py
@@ -1,13 +1,8 @@
 import streamlit as st
 
-# Initialiser l'état du bouton dans session_state si ce n'est pas déjà fait
-if "button_clicked" not in st.session_state:
-    st.session_state.button_clicked = False
+st.session_state.setdefault("button_clicked", False)
 
-# Titre de l'application
 st.title("Exemple de Bouton Toggle avec Streamlit")
-
-# CSS pour personnaliser le bouton en rouge
 st.markdown("""
     <style>
     .stButton > button {
@@ -20,11 +15,8 @@ st.markdown("""
     </style>
     """, unsafe_allow_html=True)
 
-# Afficher le bouton avec fonctionnalité toggle
 if st.button("Cliquez-moi !"):
-    # Inverser l'état du bouton dans session_state
     st.session_state.button_clicked = not st.session_state.button_clicked
 
-# Afficher ou masquer "Hello World" en fonction de l'état du bouton
 if st.session_state.button_clicked:
     st.write("Hello World!")
