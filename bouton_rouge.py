42726c924fb5d9582282947bcf2f2c05f690ce2f
Armand3125 <armandjulien05@gmail.com>
2024-11-07 11:15:16 +0100

    Update bouton_rouge.py

--- bouton_rouge.py
+++ bouton_rouge.py
@@ -24,15 +24,27 @@ css = """
 """
 st.markdown(css, unsafe_allow_html=True)
 
-# Créer une seule colonne pour les sélections de couleur
+# Créer une colonne pour les sélections de couleur
 color_options = [f"{name}" for name in pal.keys()]
 selected_color_name = st.radio("Choisissez une couleur", color_options)
 
-# Affichage de la couleur sélectionnée et du carré de couleur
-if selected_color_name:
-    rgb = pal[selected_color_name]
-    st.write(f"Vous avez sélectionné la couleur : {selected_color_name}")
-    st.markdown(
-        f"<div style='background-color: rgb{rgb}; width: 50px; height: 50px; border-radius: 5px;'></div>",
-        unsafe_allow_html=True
-    )
+# Créer une colonne à côté pour afficher la couleur correspondante
+cols = st.columns(2)
+
+with cols[1]:
+    if selected_color_name:
+        rgb = pal[selected_color_name]
+        st.write(f"Vous avez sélectionné la couleur : {selected_color_name}")
+        st.markdown(
+            f"<div style='background-color: rgb{rgb}; width: 50px; height: 50px; border-radius: 5px;'></div>",
+            unsafe_allow_html=True
+        )
+
+# Affichage des couleurs possibles avec leurs petits carrés
+with cols[0]:
+    st.write("Couleurs disponibles :")
+    for color_name, color_rgb in pal.items():
+        st.markdown(
+            f"- {color_name} : <div style='background-color: rgb{color_rgb}; width: 20px; height: 20px; display: inline-block; border-radius: 3px;'></div>",
+            unsafe_allow_html=True
+        )
