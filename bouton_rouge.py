d1d094862da52b343874cb833e6cba3665b093c4
Armand3125 <armandjulien05@gmail.com>
2024-11-07 13:46:05 +0100

    Update bouton_rouge.py

--- bouton_rouge.py
+++ bouton_rouge.py
@@ -22,7 +22,7 @@ css = """
 """
 st.markdown(css, unsafe_allow_html=True)
 
-cols = st.columns([1, 1, 1, 1])
+cols = st.columns([1, 1, 1, 1, 1, 1])
 
 with cols[0]:
     color_options = [name for name in pal.keys()]
@@ -45,6 +45,16 @@ with cols[3]:
             unsafe_allow_html=True
         )
 
+with cols[4]:
+    selected_color_name_3 = st.radio("", color_options, key="radio_3")
+
+with cols[5]:
+    for color_name, color_rgb in pal.items():
+        st.markdown(
+            f"<div style='background-color: rgb{color_rgb}; width: 50px; height: 20px; border-radius: 5px; margin-bottom: 4px;'></div>",
+            unsafe_allow_html=True
+        )
+
 if selected_color_name_1:
     rgb1 = pal[selected_color_name_1]
     st.write(f"Vous avez sélectionné la couleur (ensemble 1) : {selected_color_name_1}")
@@ -58,5 +68,13 @@ if selected_color_name_2:
     st.write(f"Vous avez sélectionné la couleur (ensemble 2) : {selected_color_name_2}")
     st.markdown(
         f"<div style='background-color: rgb{rgb2}; width: 50px; height: 50px; border-radius: 5px;'></div>",
+        unsafe_allow_html=True
+    )
+
+if selected_color_name_3:
+    rgb3 = pal[selected_color_name_3]
+    st.write(f"Vous avez sélectionné la couleur (ensemble 3) : {selected_color_name_3}")
+    st.markdown(
+        f"<div style='background-color: rgb{rgb3}; width: 50px; height: 50px; border-radius: 5px;'></div>",
         unsafe_allow_html=True
     )
