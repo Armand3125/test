864384e5dc0fab76299ecd561f948741da991c25
Armand3125 <armandjulien05@gmail.com>
2024-11-07 10:55:22 +0100

    Update bouton_rouge.py

--- bouton_rouge.py
+++ bouton_rouge.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-# Définition de la palette de couleurs
 pal = {
     "Noir Charbon": (0, 0, 0), "Blanc Jade": (255, 255, 255),
     "Jaune Or": (228, 189, 104), "Bleu Cyan": (0, 134, 214),
@@ -11,20 +10,14 @@ pal = {
     "Gris Argent": (166, 169, 170), "Violet Basic": (94, 67, 183),
 }
 
-# Titre de l'application
 st.title("Sélection de Couleurs")
 
-# Création de 4 colonnes pour les systèmes de choix de couleur
 cols = st.columns(4)
 
-# Affichage de quatre systèmes de choix de couleur indépendants
 for i, col in enumerate(cols):
     with col:
-        # Affichage d'un radio bouton pour chaque colonne
         color_options = [f"{name}" for name in pal.keys()]
         selected_color_name = st.radio(f"Choisissez une couleur ({i+1})", color_options, key=f"color_select_{i}")
-
-        # Afficher la couleur sélectionnée avec un carré de couleur
         if selected_color_name:
             rgb = pal[selected_color_name]
             st.write(f"Vous avez sélectionné la couleur : {selected_color_name}")
