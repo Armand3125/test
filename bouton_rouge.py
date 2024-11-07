1743a43d4a022cc89c3f108bdb211af2480b554c
Armand3125 <armandjulien05@gmail.com>
2024-11-07 14:05:13 +0100

    Update bouton_rouge.py

--- bouton_rouge.py
+++ bouton_rouge.py
@@ -14,7 +14,7 @@ st.title("Sélection de Couleurs")
 
 css = """
     <style>
-        .stCheckbox label {
+        .stRadio label {
             color: black;
             font-weight: normal;
         }
@@ -24,12 +24,9 @@ st.markdown(css, unsafe_allow_html=True)
 
 cols = st.columns([1, 1, 1, 1, 1, 1, 1, 1])
 
-# Utilisation des cases à cocher au lieu des boutons radio
-selected_colors_1 = []
 with cols[0]:
-    for color_name in pal.keys():
-        if st.checkbox(color_name, key=f"checkbox_1_{color_name}"):
-            selected_colors_1.append(color_name)
+    color_options = [name for name in pal.keys()]
+    selected_color_name_1 = st.radio("", color_options, key="radio_1")
 
 with cols[1]:
     for color_name, color_rgb in pal.items():
@@ -38,11 +35,8 @@ with cols[1]:
             unsafe_allow_html=True
         )
 
-selected_colors_2 = []
 with cols[2]:
-    for color_name in pal.keys():
-        if st.checkbox(color_name, key=f"checkbox_2_{color_name}"):
-            selected_colors_2.append(color_name)
+    selected_color_name_2 = st.radio("", color_options, key="radio_2")
 
 with cols[3]:
     for color_name, color_rgb in pal.items():
@@ -51,10 +45,54 @@ with cols[3]:
             unsafe_allow_html=True
         )
 
-# Répéter pour d'autres ensembles si nécessaire
-# Afficher les couleurs sélectionnées
-if selected_colors_1:
-    st.write(f"Vous avez sélectionné ces couleurs pour l'ensemble 1 : {', '.join(selected_colors_1)}")
-    
-if selected_colors_2:
-    st.write(f"Vous avez sélectionné ces couleurs pour l'ensemble 2 : {', '.join(selected_colors_2)}")
+with cols[4]:
+    selected_color_name_3 = st.radio("", color_options, key="radio_3")
+
+with cols[5]:
+    for color_name, color_rgb in pal.items():
+        st.markdown(
+            f"<div style='background-color: rgb{color_rgb}; width: 50px; height: 20px; border-radius: 5px; margin-bottom: 4px;'></div>",
+            unsafe_allow_html=True
+        )
+
+with cols[6]:
+    selected_color_name_4 = st.radio("", color_options, key="radio_4")
+
+with cols[7]:
+    for color_name, color_rgb in pal.items():
+        st.markdown(
+            f"<div style='background-color: rgb{color_rgb}; width: 50px; height: 20px; border-radius: 5px; margin-bottom: 4px;'></div>",
+            unsafe_allow_html=True
+        )
+
+if selected_color_name_1:
+    rgb1 = pal[selected_color_name_1]
+    st.write(f"Vous avez sélectionné la couleur (ensemble 1) : {selected_color_name_1}")
+    st.markdown(
+        f"<div style='background-color: rgb{rgb1}; width: 50px; height: 50px; border-radius: 5px;'></div>",
+        unsafe_allow_html=True
+    )
+
+if selected_color_name_2:
+    rgb2 = pal[selected_color_name_2]
+    st.write(f"Vous avez sélectionné la couleur (ensemble 2) : {selected_color_name_2}")
+    st.markdown(
+        f"<div style='background-color: rgb{rgb2}; width: 50px; height: 50px; border-radius: 5px;'></div>",
+        unsafe_allow_html=True
+    )
+
+if selected_color_name_3:
+    rgb3 = pal[selected_color_name_3]
+    st.write(f"Vous avez sélectionné la couleur (ensemble 3) : {selected_color_name_3}")
+    st.markdown(
+        f"<div style='background-color: rgb{rgb3}; width: 50px; height: 50px; border-radius: 5px;'></div>",
+        unsafe_allow_html=True
+    )
+
+if selected_color_name_4:
+    rgb4 = pal[selected_color_name_4]
+    st.write(f"Vous avez sélectionné la couleur (ensemble 4) : {selected_color_name_4}")
+    st.markdown(
+        f"<div style='background-color: rgb{rgb4}; width: 50px; height: 50px; border-radius: 5px;'></div>",
+        unsafe_allow_html=True
+    ) 
