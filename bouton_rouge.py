d218e84b892e466744f699bf1765a99206aaf29b
Armand3125 <armandjulien05@gmail.com>
2024-11-07 10:53:12 +0100

    Update bouton_rouge.py

--- bouton_rouge.py
+++ bouton_rouge.py
@@ -14,15 +14,21 @@ pal = {
 # Titre de l'application
 st.title("Sélection de Couleurs")
 
-# Affichage des options de couleur dans une radio
-color_options = [f"{name}" for name in pal.keys()]
-selected_color_name = st.radio("Choisissez une couleur", color_options)
+# Création de 4 colonnes pour les systèmes de choix de couleur
+cols = st.columns(4)
 
-# Afficher la couleur sélectionnée avec un carré de couleur
-if selected_color_name:
-    rgb = pal[selected_color_name]
-    st.write(f"Vous avez sélectionné la couleur : {selected_color_name}")
-    st.markdown(
-        f"<div style='background-color: rgb{rgb}; width: 50px; height: 50px; border-radius: 5px;'></div>",
-        unsafe_allow_html=True
-    ) 
+# Affichage de quatre systèmes de choix de couleur indépendants
+for i, col in enumerate(cols):
+    with col:
+        # Affichage d'un radio bouton pour chaque colonne
+        color_options = [f"{name}" for name in pal.keys()]
+        selected_color_name = st.radio(f"Choisissez une couleur ({i+1})", color_options, key=f"color_select_{i}")
+
+        # Afficher la couleur sélectionnée avec un carré de couleur
+        if selected_color_name:
+            rgb = pal[selected_color_name]
+            st.write(f"Vous avez sélectionné la couleur : {selected_color_name}")
+            st.markdown(
+                f"<div style='background-color: rgb{rgb}; width: 50px; height: 50px; border-radius: 5px;'></div>",
+                unsafe_allow_html=True
+            )
