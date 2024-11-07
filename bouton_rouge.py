eb5830667f5b9715bf8294ad97cfb508651121c8
Armand3125 <armandjulien05@gmail.com>
2024-11-07 11:48:10 +0100

    Update bouton_rouge.py

--- bouton_rouge.py
+++ bouton_rouge.py
@@ -30,8 +30,8 @@ cols = st.columns([1, 1, 1, 1])  # Quatre colonnes dans une même ligne
 # Premier groupe de colonnes (1 et 2)
 with cols[0]:
     # Liste des options de couleurs avec cases à cocher sans texte
-    color_options = [f"{name}" for name in pal.keys()]
-    selected_color_name_1 = st.radio("", color_options, index=0)  # Case à cocher sans texte visible
+    color_options = list(pal.values())  # Utiliser les couleurs elles-mêmes comme options
+    selected_color_rgb_1 = st.radio("", color_options, index=0)  # Case à cocher sans texte visible
 
 with cols[1]:
     # Affichage des rectangles colorés en face des cases à cocher
@@ -45,7 +45,7 @@ with cols[1]:
 # Deuxième groupe de colonnes (3 et 4)
 with cols[2]:
     # Liste des options de couleurs avec cases à cocher sans texte
-    selected_color_name_2 = st.radio("", color_options, index=1)  # Case à cocher sans texte visible
+    selected_color_rgb_2 = st.radio("", color_options, index=1)  # Case à cocher sans texte visible
 
 with cols[3]:
     # Affichage des rectangles colorés pour le deuxième ensemble
@@ -57,19 +57,17 @@ with cols[3]:
         )
 
 # Afficher le texte et le carré de couleur sélectionnée pour le premier ensemble de colonnes
-if selected_color_name_1:
-    rgb1 = pal[selected_color_name_1]
-    st.write(f"Vous avez sélectionné la couleur (ensemble 1) : {selected_color_name_1}")
+if selected_color_rgb_1:
+    st.write(f"Vous avez sélectionné la couleur (ensemble 1):")
     st.markdown(
-        f"<div style='background-color: rgb{rgb1}; width: 50px; height: 50px; border-radius: 5px;'></div>",
+        f"<div style='background-color: rgb{selected_color_rgb_1}; width: 50px; height: 50px; border-radius: 5px;'></div>",
         unsafe_allow_html=True
     )
 
 # Afficher le texte et le carré de couleur sélectionnée pour le deuxième ensemble de colonnes
-if selected_color_name_2:
-    rgb2 = pal[selected_color_name_2]
-    st.write(f"Vous avez sélectionné la couleur (ensemble 2) : {selected_color_name_2}")
+if selected_color_rgb_2:
+    st.write(f"Vous avez sélectionné la couleur (ensemble 2):")
     st.markdown(
-        f"<div style='background-color: rgb{rgb2}; width: 50px; height: 50px; border-radius: 5px;'></div>",
+        f"<div style='background-color: rgb{selected_color_rgb_2}; width: 50px; height: 50px; border-radius: 5px;'></div>",
         unsafe_allow_html=True
     )
