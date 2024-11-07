7ab0b66271c409719faf851c3fcd43aa5e534f67
Armand3125 <armandjulien05@gmail.com>
2024-11-07 14:04:32 +0100

    Update bouton_rouge.py

--- bouton_rouge.py
+++ bouton_rouge.py
@@ -1,97 +1,60 @@
 import streamlit as st
-from sklearn.cluster import KMeans
-from scipy.spatial import distance
-from PIL import Image
-import numpy as np
 
-# Palette de couleurs
 pal = {
-    "Noir_Charbon": (0, 0, 0), "Blanc_Jade": (255, 255, 255),
-    "Jaune_Or": (228, 189, 104), "Bleu_Cyan": (0, 134, 214),
-    "Violet_Lila": (174, 150, 212), "Vert_Gui": (63, 142, 67),
-    "Rouge_Ecarlate": (222, 67, 67), "Bleu_Marine": (0, 120, 191),
-    "Orange_Mandarine": (249, 153, 99), "Vert_Galaxie": (59, 102, 94),
-    "Bleu_Glacier": (163, 216, 225), "Violet_Magenta": (236, 0, 140),
-    "Gris_Argent": (166, 169, 170), "Violet_Basic": (94, 67, 183),
+    "NC": (0, 0, 0), "BJ": (255, 255, 255),
+    "JO": (228, 189, 104), "BC": (0, 134, 214),
+    "VL": (174, 150, 212), "VG": (63, 142, 67),
+    "RE": (222, 67, 67), "BM": (0, 120, 191),
+    "OM": (249, 153, 99), "VGa": (59, 102, 94),
+    "BG": (163, 216, 225), "VM": (236, 0, 140),
+    "GA": (166, 169, 170), "VB": (94, 67, 183),
 }
 
-# Calcul des couleurs les plus proches
-def proches(c, pal):
-    dists = [(n, distance.euclidean(c, col)) for n, col in pal.items()]
-    return sorted(dists, key=lambda x: x[1])
+st.title("Sélection de Couleurs")
 
-def proches_lim(c, pal, n):
-    return [n for n, _ in proches(c, pal)[:n]]
+css = """
+    <style>
+        .stCheckbox label {
+            color: black;
+            font-weight: normal;
+        }
+    </style>
+"""
+st.markdown(css, unsafe_allow_html=True)
 
-# Créer une nouvelle image avec les couleurs sélectionnées
-def nouvelle_img(img_arr, labels, cl_proches, selected_colors, pal):
-    color_map = {i: pal[cl_proches[i][selected_colors[i]]] for i in range(len(cl_proches))}
-    img_mapped = np.array([color_map[label] for label in labels])
-    return img_mapped.reshape(img_arr.shape)
+cols = st.columns([1, 1, 1, 1, 1, 1, 1, 1])
 
-# Traiter l'image pour clustering et application de la palette
-def traiter_img(img, Nc, Nd, dim_max):
-    try:
-        img = Image.open(img).convert('RGB')
-        img.thumbnail((dim_max, dim_max))
-        img_arr = np.array(img)
+# Utilisation des cases à cocher au lieu des boutons radio
+selected_colors_1 = []
+with cols[0]:
+    for color_name in pal.keys():
+        if st.checkbox(color_name, key=f"checkbox_1_{color_name}"):
+            selected_colors_1.append(color_name)
 
-        pixels = img_arr.reshape(-1, 3)
-        kmeans = KMeans(n_clusters=Nc, random_state=0).fit(pixels)
-        labels = kmeans.labels_
+with cols[1]:
+    for color_name, color_rgb in pal.items():
+        st.markdown(
+            f"<div style='background-color: rgb{color_rgb}; width: 50px; height: 20px; border-radius: 5px; margin-bottom: 4px;'></div>",
+            unsafe_allow_html=True
+        )
 
-        uniq, counts = np.unique(labels, return_counts=True)
-        total_px = pixels.shape[0]
-        cl_counts = dict(zip(uniq, counts))
+selected_colors_2 = []
+with cols[2]:
+    for color_name in pal.keys():
+        if st.checkbox(color_name, key=f"checkbox_2_{color_name}"):
+            selected_colors_2.append(color_name)
 
-        sorted_cls = sorted(cl_counts.items(), key=lambda x: x[1], reverse=True)
-        cl_proches = [proches_lim(kmeans.cluster_centers_[i], pal, Nd) for i in cl_counts.keys()]
+with cols[3]:
+    for color_name, color_rgb in pal.items():
+        st.markdown(
+            f"<div style='background-color: rgb{color_rgb}; width: 50px; height: 20px; border-radius: 5px; margin-bottom: 4px;'></div>",
+            unsafe_allow_html=True
+        )
 
-        if 'selected_colors' not in st.session_state:
-            st.session_state.selected_colors = [0] * Nc
-        elif len(st.session_state.selected_colors) != Nc:
-            st.session_state.selected_colors = [0] * Nc
-
-        new_img_arr = nouvelle_img(img_arr, labels, cl_proches, st.session_state.selected_colors, pal)
-        st.session_state.modified_image = new_img_arr.astype('uint8')
-
-        # Interface de sélection de couleurs par checkbox
-        for idx, (cl, count) in enumerate(sorted_cls):
-            percentage = (count / total_px) * 100
-            st.write(f"Cluster {idx + 1} - {percentage:.2f}%")
-            col_options = cl_proches[cl]
-
-            # Afficher les options de couleur avec des cases à cocher
-            selected_color = st.session_state.selected_colors[cl]
-            color_keys = [pal[key] for key in col_options]
-            for j, color_rgb in enumerate(color_keys):
-                rgb_str = f"rgb({color_rgb[0]}, {color_rgb[1]}, {color_rgb[2]})"
-                if st.checkbox(f"Cluster {idx+1} - {col_options[j]}", key=f"checkbox_{idx}_{j}", value=(selected_color == j)):
-                    # Si la case est cochée, définir la couleur sélectionnée
-                    st.session_state.selected_colors[cl] = j
-                    new_img_arr = nouvelle_img(img_arr, labels, cl_proches, st.session_state.selected_colors, pal)
-                    st.session_state.modified_image = new_img_arr.astype('uint8')
-
-                # Affichage du rectangle coloré
-                st.markdown(
-                    f"<div style='background-color: {rgb_str}; width: 50px; height: 20px; border-radius: 5px; margin-bottom: 4px;'></div>",
-                    unsafe_allow_html=True
-                )
-
-    except Exception as e:
-        st.error(f"Une erreur est survenue : {e}")
-
-# Interface Streamlit
-st.title("Tylice")
-uploaded_file = st.file_uploader("Choisissez une image", type=["jpg", "jpeg", "png"])
-Nc = st.slider("Nombre de Clusters", 2, 7, 4)
-Nd = st.slider("Nombre de Couleurs dans la Palette", 2, len(pal), 6)
-
-# Fixer la dimension maximale de l'image à 400
-dim_max = 400  
-
-if uploaded_file is not None:
-    traiter_img(uploaded_file, Nc, Nd, dim_max)
-
-if 'modified_image' in st.session_state:
-    st.image(st.session_state.modified_image, caption="Image Modifiée", width=int(1.5 * dim_max))
+# Répéter pour d'autres ensembles si nécessaire
+# Afficher les couleurs sélectionnées
+if selected_colors_1:
+    st.write(f"Vous avez sélectionné ces couleurs pour l'ensemble 1 : {', '.join(selected_colors_1)}")
+    
+if selected_colors_2:
+    st.write(f"Vous avez sélectionné ces couleurs pour l'ensemble 2 : {', '.join(selected_colors_2)}")
