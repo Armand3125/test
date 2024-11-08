f79e1611713d9b8635434fce815f03e24c8470ed
Armand3125 <armandjulien05@gmail.com>
2024-11-08 15:57:18 +0100

    Update bouton_rouge.py

--- bouton_rouge.py
+++ bouton_rouge.py
@@ -25,7 +25,7 @@ css = """
             flex-direction: column;
             align-items: center;
             justify-content: center;
-            margin-top: 10px;
+            margin-top: 1px;
         }
         .color-container {
             display: flex;
